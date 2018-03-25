f0128fbc75152bdfd318bf66dac1aca27cd58cb1
Rob "Rex" Beatie <rdev1163@outlook.com>
2018-03-25 01:21:29 +0000

    Omni update

---
+++ src/services/firebase.service.ts
@@ -0,0 +1,20 @@
+import {Observable, Observer} from '@reactivex/rxjs';
+import * as fb from "firebase";
+
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDx4o2UW-UdKTVlO20JeALkFPaKHcWXw8E",
+    authDomain: "seatpeek.firebaseapp.com",
+    databaseURL: "https://seatpeek.firebaseio.com",
+    projectId: "seatpeek",
+    storageBucket: "seatpeek.appspot.com",
+    messagingSenderId: "980121901535"
+};
+
+export function onSeatChange$(): Observable<any> {
+    return Observable.create((observer: Observer<any>) => {
+        // firebase.database();
+        fb.database().ref('/seats').on('value', (update) => {
+            observer.next(update.val());
+        });
+    });
+}
